659022e425fcecb3e60bfd953db2b1d66d5190bf
Rishi Vykunta <arasavillirishi0@gmail.com>
2025-12-31 23:39:16 +0530

    Update CORS to support multiple origins and all HTTP methods

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,14 @@ const io = new Server(httpServer, {
   },
 });
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173'],
   credentials: true,
-}));
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
